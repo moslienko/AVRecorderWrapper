336606b68e1590e87729b3f6aa690682fe4bfb92
moslienko <8676976+moslienko@users.noreply.github.com>
2024-08-01 09:55:03 +0700

    Init project

--- Package.swift
+++ Package.swift
@@ -5,17 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "AVRecorderWrapper",
+    platforms: [
+        .iOS(.v12)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "AVRecorderWrapper",
             targets: ["AVRecorderWrapper"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/moslienko/AppViewUtilits.git", from: "1.2.6")
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "AVRecorderWrapper"),
+            name: "AVRecorderWrapper",
+            dependencies: ["AppViewUtilits"]
+        ),
         .testTarget(
             name: "AVRecorderWrapperTests",
             dependencies: ["AVRecorderWrapper"]),
